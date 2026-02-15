cdda4c2450961a5d029bb0611d1f107bc29c3a07
odeindev <odeindev@gmail.com>
2026-02-15 12:32:08 +0100

    Initial commit

---
+++ src/main/kotlin/org/odeindev/autounclaim/AutoUnclaim.kt
@@ -0,0 +1,14 @@
+package org.odeindev.autounclaim
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AutoUnclaim : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
